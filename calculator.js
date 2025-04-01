e965a28ae1b4efe68674016753e3558403fd79b4
Tofunmi Olowoniyi <esthertofunmiolowoniyi@gmail.com>
2025-04-01 14:54:44 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+// //calculator
+// const display = document.getElementById("display");
+
+// function taptodisplay(input){
+//    display.value += input;
+// }
+
+// function calculate(){
+
+// }
+
+// function cleardisplay(){
+
+// }
